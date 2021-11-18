50cb2ade86579b72a9361d1c50f750d363b329bc
dawkrz13 <dawkrz13@gmail.com>
2021-11-18 18:36:05 +0100

    manual implementation of Hilbert transform

--- AM_sound.m
+++ AM_sound.m
@@ -1,4 +1,4 @@
-clear all
+clc, clear all, close all
 
 % choose soung source
 recording = true;
@@ -10,10 +10,7 @@ else
 end
 y_single_channel = y(:,1)';
 
-% play original sound
-%sound(single_channel,fs)
-
-subplot(2,2,1);
+subplot(3,2,1);
 plot(y_single_channel); title('sound');
 xlabel('n [-]'); ylabel('amplitude [-]');
 xlim([0 length(y_single_channel)]);
@@ -26,31 +23,72 @@ y_single_channel_translated = y_single_channel + abs(sig_min);
 fc = 10;
 fs = 100;
 y_am = ammod(y_single_channel_translated, fc, fs);
-subplot(2,2,2);
+subplot(3,2,2);
 plot(y_am); title('modulated signal');
 xlabel('n [-]'); ylabel('amplitude [-]');
 xlim([0 length(y_single_channel)]);
 
-% Hilbert transform 
-h = hilbert(y_am);  %TODO: replace this function with our implementation
-subplot(2,2,3);
-plot(imag(h)); hold on; plot(real(h));
+%% Manual Hilbert transform
+f = fft(y_am);
+n = length(y_am);
+
+% create a copy that is multiplied by the complex operator
+complexf = 1i*f;
+
+% find indices of positive and negative frequencies
+posF = 2:floor(n/2)+mod(n,2);
+negF = ceil(n/2)+1+~mod(n,2):n;
+
+% rotate Fourier coefficients
+f(posF) = f(posF) + -1i*complexf(posF);
+f(negF) = f(negF) +  1i*complexf(negF);
+
+% take inverse FFT
+hilbertx = ifft(f);
+imh2 = imag(hilbertx);
+reh2 = real(hilbertx);
+
+subplot(3,2,3);
+plot(imag(hilbertx)); hold on; plot(real(hilbertx));
 legend('real','imaginary')
-title('Hilbert function')
+title('Manual Hilbert function')
+xlabel('n [-]'); ylabel('amplitude [-]');
+xlim([0 length(y_single_channel)]);
+
+% signal demodulation
+y_dem_translated2 = abs(hilbertx); 
+
+% signal postprocessing
+y_dem2 = y_dem_translated2 - abs(sig_min);
+
+subplot(3,2,4);
+plot(y_dem2); title('Manual demodulated signal');
+xlabel('n [-]'); ylabel('amplitude [-]');
+xlim([0 length(y_single_channel)]);
+
+%% Matlab Hilbert transform 
+h = hilbert(y_am);
+subplot(3,2,5);
+plot(imag(h)); hold on; plot(real(h));
+legend('real','imaginary');
+title('Matlab Hilbert function');
 xlabel('n [-]'); ylabel('amplitude [-]');
 xlim([0 length(y_single_channel)]);
 
 % AM signal demodulation
-y_dem_translated = abs(h); 
-%y_dem_translated = sqrt(imh.^2 + reh.^2);
+y_dem_translated = abs(h);
 
 % signal postprocessing
 y_dem = y_dem_translated - abs(sig_min);
 
-subplot(2,2,4);
+subplot(3,2,6);
 plot(y_dem); title('demodulated signal');
 xlabel('n [-]'); ylabel('amplitude [-]');
 xlim([0 length(y_single_channel)]);
 
-% play demodulated AM signal
+%% compare sound
+sound(y,Fs)
+pause(6)
+sound(y_dem2,Fs)
+pause(6)
 sound(y_dem,Fs)
--- FM_sound.m
+++ FM_sound.m
@@ -1,6 +1,6 @@
-clear all
+clc, clear all, close all
 
-% choose soung source
+% choose sound source
 recording = true;
 
 if recording == true
@@ -13,7 +13,7 @@ y_single_channel = y(:,1)';
 % play original sound
 %sound(single_channel,fs)
 
-subplot(2,2,1);
+subplot(3,2,1);
 plot(y_single_channel); title('sound');
 xlabel('n [-]'); ylabel('amplitude [-]');
 xlim([0 length(y_single_channel)]);
@@ -28,17 +28,54 @@ fs = 1000;
 fd = 50;
 %y_fm = fmmod(y_single_channel_translated, fc, fs, fd);
 y_fm = fmmod(y_single_channel, fc, fs, fd);
-subplot(2,2,2);
+subplot(3,2,2);
 plot(y_fm); title('modulated signal');
 xlabel('n [-]'); ylabel('amplitude [-]');
 xlim([0 length(y_single_channel)]);
 
-% Hilbert transform 
-h = hilbert(y_fm);  %TODO: replace this function with our implementation
-subplot(2,2,3);
+%% Manual Hilbert transform
+f = fft(y_fm);
+n = length(y_fm);
+
+% create a copy that is multiplied by the complex operator
+complexf = 1i*f;
+
+% find indices of positive and negative frequencies
+posF = 2:floor(n/2)+mod(n,2);
+negF = ceil(n/2)+1+~mod(n,2):n;
+
+% rotate Fourier coefficients
+f(posF) = f(posF) + -1i*complexf(posF);
+f(negF) = f(negF) +  1i*complexf(negF);
+
+% take inverse FFT
+hilbertx = ifft(f);
+imh2 = imag(hilbertx);
+reh2 = real(hilbertx);
+
+subplot(3,2,3);
+plot(imh2); hold on; plot(reh2);
+legend('real','imaginary')
+title('Manual Hilbert function')
+xlabel('n [-]'); ylabel('amplitude [-]');
+xlim([0 length(y_single_channel)]);
+
+% FM signal demodulation
+inst_phase2 = unwrap(angle(hilbertx));
+inst_freq2 = (diff(inst_phase2)*Fs)/(2*pi);
+
+subplot(3,2,4);
+plot(inst_freq2); title('Manual demodulated signal');
+xlabel('n [-]'); ylabel('frequency [Hz]');
+xlim([0 length(y_single_channel)]);
+ylim([9350 9850]);
+
+%% Matlab Hilbert transform 
+h = hilbert(y_fm);
+subplot(3,2,5);
 plot(imag(h)); hold on; plot(real(h));
 legend('real','imaginary')
-title('Hilbert function')
+title('Matlab Hilbert function')
 xlabel('n [-]'); ylabel('amplitude [-]');
 xlim([0 length(y_single_channel)]);
 
@@ -49,12 +86,16 @@ inst_freq = (diff(inst_phase)*Fs)/(2*pi);
 % signal postprocessing
 %y_dem = y_dem_translated - abs(sig_min);
 
-subplot(2,2,4);
+subplot(3,2,6);
 %plot(y_dem); title('demodulated signal');
 plot(inst_freq); title('demodulated signal');
 xlabel('n [-]'); ylabel('frequency [Hz]');
 xlim([0 length(y_single_channel)]);
 ylim([9350 9850]);
 
-% play demodulated AM signal
-sound(inst_freq,Fs)
+%% compare sound
+sound(y_single_channel,Fs)
+pause(6)
+sound(inst_freq2/Fs,Fs)
+pause(6)
+sound(inst_freq/Fs,Fs)
